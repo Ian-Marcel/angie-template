7026c8d47163628ba50a2587c66396dd4e9d153d
Ian <comercial@ianmarcel.dev>
2026-07-05 00:21:02 -0300

    Update build script to conditionally handle deploy mode and clean up images
    Adds test image build for non-deploy scenarios and removes all tags of pushed images after deployment

--- build.sh
+++ build.sh
@@ -4,9 +4,12 @@ docker pull --quiet docker.angie.software/angie:templated
 
 docker run -it --rm docker.angie.software/angie:templated angie -v | grep -i 'Angie version' | sed 's/[^0-9.]//g' >/tmp/angie-plus-version
 
-docker build . --quiet --tag dockerizedian/angie-template-plus:$(cat /tmp/angie-plus-version)
-docker build . --quiet --tag dockerizedian/angie-template-plus:latest
-
 if [ ${1:-} = deploy ]; then
+    docker build . --quiet --tag dockerizedian/angie-template-plus:$(cat /tmp/angie-plus-version)
+    docker build . --quiet --tag dockerizedian/angie-template-plus:latest
+
     docker push --all-tags --quiet dockerizedian/angie-template-plus
+    docker image rm --all-tags dockerizedian/angie-template-plus
+else
+    docker build . --tag angie-template-plus-test_image
 fi
